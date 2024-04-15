e8809f03fef8a1319689d7267087b1cc212f3af4
Fawaz Alzamel <fawazzamel1@gmail.com>
2024-04-15 10:21:19 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿//Task 1 of LINQ
+using System;
+using System.Linq;
+
+
+
+class evenNumFunction
+{
+    static void Main()
+    {
+        int[] numbers = { 1, 2, 3, 4, 5 };
+
+        var evenNumbers = numbers.Where(n => n % 2 == 0);
+
+        foreach (var number in evenNumbers)
+        {
+            Console.WriteLine(number);
+        }
+    }
+}
+
+//Task 2 LINQ
+
+class avgStudentGrades
+{
+    static void Main()
+    {
+
+        int[] grades = { 52, 82, 63, 94, 95 };
+
+
+        var avgGrades = grades.Average();
+        Console.WriteLine(avgGrades);
+    }
+
+}
+
+//Task Functions 1
+
+
+class Greeting
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("What is your name?");
+        string name = Console.ReadLine();
+
+        GreetUser(name);
+    }
+
+    static void GreetUser(string name)
+    {
+        Console.WriteLine($"Hello {name}");
+    }
+}
+
+
+//Task 2 Functions
+
+class calculateVolume { 
+    static void Main() {
+
+        Console.WriteLine("What is your length?");
+        string lengthInput = Console.ReadLine();
+
+        double.TryParse(lengthInput, out var length);
+
+        Console.WriteLine("What is your width?");
+        string widthInput = Console.ReadLine();
+
+        double.TryParse(lengthInput, out var width);
+
+        Console.WriteLine("What is your height?");
+        string heightInput = Console.ReadLine();
+
+        double.TryParse(lengthInput, out var height);
+
+        Console.WriteLine($"The Volume is {length*width*height}");
+       
+    }
+
+    
+}
